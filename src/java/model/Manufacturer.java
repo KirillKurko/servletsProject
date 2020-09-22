c984c97b003aed17bfbad48be4d4931ee9a2626d
Кирилл Курко <kirillkurko2609@gmail.com>
2020-09-22 10:10:14 +0300

    Manufacturer implemented

--- src/java/model/Manufacturer.java
+++ src/java/model/Manufacturer.java
@@ -1,4 +1,43 @@
 package java.model;
 
 public class Manufacturer {
+
+    private int id;
+    private String name;
+    private String country;
+
+    public Manufacturer(int id, String name, String country) {
+        this.id = id;
+        this.name = name;
+        this.country = country;
+    }
+
+    public Manufacturer(String name, String country) {
+        this.name = name;
+        this.country = country;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
