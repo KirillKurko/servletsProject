34f1665ca575f1deced724f9c915cf60b2b32ba8
Кирилл Курко <kirillkurko2609@gmail.com>
2020-09-22 10:06:14 +0300

    Souvenir implemented

--- src/java/model/Souvenir.java
+++ src/java/model/Souvenir.java
@@ -7,6 +7,61 @@ public class Souvenir {
     private int id;
     private String name;
     private String manufacturerEmail;
-    Calendar date;
+    private Calendar manufacturingDate;
     private double price;
+
+    public Souvenir(int id, String name, String manufacturerEmail, Calendar manufacturingDate, double price) {
+        this.id = id;
+        this.name = name;
+        this.manufacturerEmail = manufacturerEmail;
+        this.manufacturingDate = manufacturingDate;
+        this.price = price;
+    }
+
+    public Souvenir(String name, String manufacturerEmail, Calendar manufacturingDate, double price) {
+        this.name = name;
+        this.manufacturerEmail = manufacturerEmail;
+        this.manufacturingDate = manufacturingDate;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getManufacturerEmail() {
+        return manufacturerEmail;
+    }
+
+    public Calendar getManufacturingDate() {
+        return manufacturingDate;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setManufacturerEmail(String manufacturerEmail) {
+        this.manufacturerEmail = manufacturerEmail;
+    }
+
+    public void setManufacturingDate(Calendar manufacturingDate) {
+        this.manufacturingDate = manufacturingDate;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
